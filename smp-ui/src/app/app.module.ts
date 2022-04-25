5d67d02b28e04a927a94f4bdd589583679524acc
WEN SHUFA <shufa17@gmail.com>
2022-04-25 16:30:24 +0800

    create components: header, admin, user, home, login, forbidden and create two empty folder

--- smp-ui/src/app/app.module.ts
+++ smp-ui/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { HearderComponent } from './hearder/hearder.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    UserComponent,
+    LoginComponent,
+    HearderComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,
